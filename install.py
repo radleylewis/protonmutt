c2dd2550611faa07539dc23224265984a2c3c10c
Radley E Siwell-Lewis <radley.lewis@gmail.com>
2023-07-16 14:49:10 +0700

    feat: break up muttrc files

--- install.py
+++ install.py
@@ -6,18 +6,8 @@ import shutil
 from getpass import getpass
 
 
-MUTT_CONFIG_DIRECTORY = os.path.expanduser("~/.config/mutt")
-DEPENDENCIES = {"neomutt"}
-
-
-# def install_dependencies():
-#     for package in DEPENDENCIES:
-#         if not is_installed(package):
-#             print(f"Installing {package}...")
-#             subprocess.run(["sudo", "pacman", "-Syu", package], check=True)
-#         else:
-#             print(f"{package} is already installed. Skipping...")
-#
+MUTT_CONFIG_DIRECTORY = os.path.expanduser("~/.mutt")
+REQUIRED_DEPENDENCIES = {"neomutt"}
 
 
 class ProtonMutt:
@@ -33,6 +23,11 @@ class ProtonMutt:
         dest_of_file = os.path.join(MUTT_CONFIG_DIRECTORY, name)
         shutil.copyfile(file_to_copy, dest_of_file)
 
+    def _move_file(self, dir: str, name: str) -> None:
+        file_to_copy = os.path.join(os.getcwd(), f"./{dir}/{name}")
+        dest_of_file = os.path.join(MUTT_CONFIG_DIRECTORY, name)
+        shutil.move(file_to_copy, dest_of_file)
+
     @property
     def _smtp_url(self) -> str:
         return f"smtp://{self.user_email}:{self.smtp_pass}@localhost:{self.smtp_port}"
@@ -41,20 +36,32 @@ class ProtonMutt:
     def _folder(self) -> str:
         return f"imap://localhost:{self.imap_port}"
 
+    def check_dependencies(self) -> None:
+        for package in REQUIRED_DEPENDENCIES:
+            if not is_installed(package):
+                print(f"Installing {package}...")
+                subprocess.run(["sudo", "pacman", "-Syu", package], check=True)
+            else:
+                print(f"{package} is already installed. Skipping...")
+
+    def create_tmp_directory(self) -> None:
+        if os.path.exists("./tmp"):
+            shutil.rmtree("./tmp")
+        else:
+            os.makedirs("./tmp")
+
     def create_mutt_directories(self) -> None:
         if os.path.exists(MUTT_CONFIG_DIRECTORY):
-            text = (
-                "The '.config/mutt' dir already exists. Do you want to proceed? (y/N): "
-            )
+            text = "The '.mutt' dir already exists. Do you want to proceed? (y/N): "
             if input(text).lower() not in ["y", "yes"]:
                 raise Exception("User canceled the process.")
         else:
             os.makedirs(MUTT_CONFIG_DIRECTORY)
             os.makedirs(MUTT_CONFIG_DIRECTORY + "/bodies")
-            print("The '.config/mutt' directory has been created successfully.")
+            print("The './mutt' directory has been created successfully.")
 
     def get_user_info(self) -> None:
-        print("\nPlease enter your proton bridge credentials.\n")
+        print("\nPlease enter your proton bridge protonmail.\n")
         self.user_email = input("Email address: ")
         self.real_name = input("Full name: ")
         self.imap_port = input("Proton bridge imap port (defaults to 1143): ") or 1143
@@ -67,41 +74,57 @@ class ProtonMutt:
         with open(muttrc_path, "w") as muttrc:
             muttrc.write(f'set realname = "{self.real_name}"\n')
             muttrc.write('set pgp_default_key = "<gpg key identifier>"\n')
-            muttrc.write('source "gpg -dq ~/.config/mutt/credentials.gpg |"\n')
+            muttrc.write('source "gpg -dq ~/.mutt/protonmail.gpg |"\n')
             muttrc.write(f'set pgp_sign_as = "{self.real_name}"\n')
             muttrc.write("set pgp_use_gpg_agent\n")
-
             muttrc.write(f'set from = "{self.user_email}"\n')
-            muttrc.write(f'set folder = "{self._folder}"\n')
-            muttrc.write(f'set spoolfile = "{self._folder}/INBOX"\n')
-            muttrc.write(f'set postponed = "{self._folder}/[Protonmail]/Drafts"\n')
-            muttrc.write(f'set record = "{self._folder}/[Protonmail]/Sent Mail"\n')
-            muttrc.write(f'set trash = "{self._folder}/[Protonmail]/Trash"\n')
-            muttrc.write(f'set mbox = "{self._folder}/[Protonmail]/All Mail"\n')
             muttrc.write('mailboxes =Starred =Drafts =Sent =Spam =Trash"\n')
-            muttrc.write("source muttrc.base\n")
+            muttrc.write("set mailcap_path = $HOME/.mutt/mailcap\n")
+            muttrc.write("source muttrc.general_settings\n")
+            muttrc.write("source muttrc.key_bindings\n")
             muttrc.write("source muttrc.colour_scheme\n")
 
-    def create_credentials(self) -> None:
-        credentials_path = os.path.join(os.getcwd(), "./tmp/credentials")
-        with open(credentials_path, "w") as credentials:
-            credentials.write(f'set imap_pass = "{self.imap_pass}"\n')
-            credentials.write(f'set imap_user = "{self.user_email}"\n')
-            credentials.write(f'set smtp_url = "{self._smtp_url}"\n')
+    def setup_protonmail(self) -> None:
+        protonmail_path = os.path.join(os.getcwd(), "./tmp/protonmail")
+        with open(protonmail_path, "w") as protonmail:
+            protonmail.write("set ssl_starttls = yes\n")
+            protonmail.write("set ssl_force_tls = yes\n")
+            protonmail.write('set send_charset = "us-ascii:utf-8"\n')
+            protonmail.write(f'set imap_pass = "{self.imap_pass}"\n')
+            protonmail.write(f'set imap_user = "{self.user_email}"\n')
+            protonmail.write(f'set smtp_url = "{self._smtp_url}"\n')
+            protonmail.write(f'set folder = "{self._folder}"\n')
+            protonmail.write(f'set folder = "{self._folder}"\n')
+            protonmail.write(f'set spoolfile = "{self._folder}/INBOX"\n')
+            protonmail.write(f'set postponed = "{self._folder}/[Protonmail]/Drafts"\n')
+            protonmail.write(f'set record = "{self._folder}/[Protonmail]/Sent Mail"\n')
+            protonmail.write(f'set trash = "{self._folder}/[Protonmail]/Trash"\n')
+            protonmail.write(f'set mbox = "{self._folder}/[Protonmail]/All Mail"\n')
+            protonmail.write('set header_cache = "~/.mutt/cache/headers"\n')
+            protonmail.write('set message_cachedir = "~/.mutt/cache/bodies"\n')
+            protonmail.write('set certificate_file = "~/.mutt/certificates"\n')
+            protonmail.write('set smtp_authenticators = "gssapi:login"\n')
+            protonmail.write("set imap_keepalive = 900\n")
+            protonmail.write(f'set from = "{self.user_email}"\n')
+            protonmail.write(f'set realname = "{self.real_name}"\n')
+            protonmail.write("set use_from = yes\n")
+            protonmail.write("unset use_domain\n")
 
-    def sign_credentials(self) -> None:
+    def sign_protonmail(self) -> None:
         os.seteuid(os.getuid())  # Temporarily change the euid to effective user ID
+
         subprocess.run(
-            ["gpg", "--sign", "./tmp/credentials"], capture_output=True, text=True
+            ["gpg", "--sign", "./tmp/protonmail"], capture_output=True, text=True
         )
         os.seteuid(os.geteuid())  # Restore the effective user ID
 
     def setup_neomutt(self) -> None:
-        self._copy_file("tmp", "muttrc")
-        self._copy_file("tmp", "credentials.gpg")
+        self._move_file("tmp", "muttrc")
+        self._move_file("tmp", "protonmail.gpg")
         self._copy_file("config", "signature")
         self._copy_file("config", "muttrc.colour_scheme")
-        self._copy_file("config", "muttrc.base")
+        self._copy_file("config", "muttrc.key_bindings")
+        self._copy_file("config", "muttrc.general_settings")
         self._copy_file("config", "mailcap")
 
     def cleanup(self) -> None:
@@ -118,10 +141,12 @@ def is_installed(package):
 
 if __name__ == "__main__":
     proton_mutt = ProtonMutt()
+    proton_mutt.check_dependencies()
+    proton_mutt.create_tmp_directory()
     proton_mutt.create_mutt_directories()
     proton_mutt.get_user_info()
     proton_mutt.create_muttrc()
-    proton_mutt.create_credentials()
-    proton_mutt.sign_credentials()
+    proton_mutt.setup_protonmail()
+    proton_mutt.sign_protonmail()
     proton_mutt.setup_neomutt()
     proton_mutt.cleanup()
